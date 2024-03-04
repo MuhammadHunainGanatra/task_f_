01c4edf749acfe93d4bc69e39b18718afff5fa98
Muhammad Hunain <153491211+MuhammadHunainGanatra@users.noreply.github.com>
2024-03-04 13:05:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+// var weekday = prompt("Plz Enter Your Weekday")
+// if(weekday == 1){
+//     console.log("January")
+// }
+// else if(weekday == 2){
+//     console.log("Fedruary")
+// }
+// else if(weekday == 3){
+//     console.log("March")
+// }
+// else if(weekday == 4){
+//     console.log("April")
+// }
+// else if(weekday == 5){
+//     console.log("May")
+// }
+// else if(weekday == 6){
+//     console.log("June")
+// }
+// else if(weekday == 7){
+//     console.log("Sunday")
+// }
+// else{
+//     console.log("Plz Enter number between 1 to 7")
+// }
+
+
+
+
+// var age = prompt('Enter Age');
+// var res = 'U.S.' ;
+// if(age > 20 && age < 15 && res === 'U.S.'){
+//     console.log('Yes')
+// }
+// else{
+//     console.log('NO')
+// }
+
+
+
+// var number1 = prompt('ENter number');
+// if(number1%2 == 0 && number1%3 == 0){
+//   console.log('yes');
+// }
+// else{
+//     console.log('No')
+// }
+
+
+
+// var practical = prompt("enter practical marks");
+// var theory = prompt("enter your theory marks");
+// if(practical >= 60 || theory >= 60){
+//     alert('you are pass')
+// }
+// else{
+//     alert('you are fail')
+// }
+
+
+
+// var age = prompt('Enter your age');
+// var idcard = prompt('Do you have vaild ID Card?');
+// if(age >= 18 && idcard === 'yes'){
+//     alert('you are ready for vote')
+// }
+// else{
+//     alert('you are not ready for vote')
+// }
+
+
+// var number = +prompt("Enter");
+// if(number % 4 == 0 && number % 100 != 0){
+//     console.log(true)
+// } else{
+//     console.log(false);
+// }
+
+
+
+
+// var side1 = 4;
+// var side2 = 5;
+// var side3 = 7;
+
+// if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+//     console.log("Valid triangle");
+// } else {
+//     console.log("Invalid triangle");
+// }
+
+
+
+
+
+var city = prompt('enter your city name');
+if(city < 0.25){
+    console.log('Please refill the fuel in your car')
+}
